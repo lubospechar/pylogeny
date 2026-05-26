f67940cdfac0345cbb6bcf1cde7b3eaa69f27eb8
Luboš Pechar <lubos.pechar@prirodou.cz>
2026-05-26 14:03:27 +0200

    Add "florapy" Django app with initial setup and GIS database configuration adjustments

--- pylogeny/settings.py
+++ pylogeny/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'mptt',
     'pylogenyapp'
 ]
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'pylogeny.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        "ENGINE": "django.contrib.gis.db.backends.spatialite",
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
