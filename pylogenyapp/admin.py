638e0b423c744bf0f012e1a01957e87b128a95ee
Luboš Pechar <lubos.pechar@prirodou.cz>
2026-05-26 12:01:03 +0200

    Add models TaxonomicRank and Taxon with migrations and admin integration in "pylogenyapp" Django app

--- pylogenyapp/admin.py
+++ pylogenyapp/admin.py
@@ -1,3 +1,77 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
 
-# Register your models here.
+from pylogenyapp.models import TaxonomicRank, Taxon
+
+
+@admin.register(TaxonomicRank)
+class TaxonomicRankAdmin(admin.ModelAdmin):
+    list_display = ("level", "code", "name_cs")
+    list_display_links = ("code", "name_cs")
+    search_fields = ("code", "name_cs")
+    list_filter = ("level",)
+    ordering = ("level",)
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("name_cs", "code", "level"),
+            },
+        ),
+    )
+
+@admin.register(Taxon)
+class TaxonAdmin(DraggableMPTTAdmin):
+
+
+    list_display = (
+        "tree_actions",
+        "indented_title",
+        "name_cs",
+        "taxonomic_rank",
+    )
+    list_display_links = (
+        "indented_title",
+    )
+    search_fields = (
+        "scientific_name",
+        "name_cs",
+    )
+
+    autocomplete_fields = (
+        "parent",
+    )
+
+    list_filter = (
+        "taxonomic_rank",
+    )
+    ordering = (
+        "tree_id",
+        "lft",
+    )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "parent",
+                    "taxonomic_rank",
+                    "scientific_name",
+                    "name_cs",
+                ),
+            },
+        ),
+    )
+
+    @admin.display(description="Scientific name")
+    def indented_title(self, obj):
+        return format_html(
+            '<div style="text-indent: {indent}px">{title}</div>',
+            indent=obj.get_level() * self.mptt_level_indent,
+            title=obj.scientific_name,
+        )
+
+    indented_title.short_description = "Scientific name"
--- pylogenyapp/models.py
+++ pylogenyapp/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
 
-# Create your models here.
+
+class TaxonomicRank(models.Model):
+    name_cs = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name=_("Name czech"),
+    )
+    code = models.SlugField(
+        max_length=100,
+        unique=True,
+        verbose_name=_("Code"),
+        help_text=_("For example: species, genus, family, order"),
+    )
+    level = models.PositiveSmallIntegerField(
+        unique=True,
+        verbose_name=_("Level"),
+        help_text=_("Lower number means a more general rank."),
+    )
+
+    class Meta:
+        verbose_name = _("Taxonomic rank")
+        verbose_name_plural = _("Taxonomic ranks")
+        ordering = ["level"]
+
+    def __str__(self):
+        return self.code
+
+
+class Taxon(MPTTModel):
+    name_cs = models.CharField(
+        max_length=255,
+        verbose_name=_("Name czech"),
+    )
+    scientific_name = models.CharField(
+        max_length=255,
+        verbose_name=_("Scientific name"),
+    )
+    taxonomic_rank = models.ForeignKey(
+        TaxonomicRank,
+        on_delete=models.PROTECT,
+        related_name="taxa",
+        verbose_name=_("Taxonomic rank"),
+    )
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="children",
+        verbose_name=_("Parent taxon"),
+    )
+
+    def save(self, *args, **kwargs):
+        if self.name_cs:
+            self.name_cs = self.name_cs.lower()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.scientific_name} ({self.taxonomic_rank})'
+
+
+    class MPTTMeta:
+        order_insertion_by = ["scientific_name"]
+
+    class Meta:
+        verbose_name = _("Taxon")
+        verbose_name_plural = _("Taxa")
+        ordering = ["tree_id", "lft"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["scientific_name", "taxonomic_rank", "parent"],
+                name="unique_taxon_scientific_name_rank_parent",
+            ),
+        ]
